6f9a53e4ec37644661c896b5f1942358ccc31c49
Clodoaldo Favero <clodoaldofavero@gmail.com>
2017-09-12 15:20:00 -0300

    Tela inicial e lanca bolim animado ok.

--- bocha/lib.h
+++ bocha/lib.h
@@ -1,15 +1,17 @@
 #include <stdlib.h>
-#define N 10
+#define TAMANHO 10
+#include <windows.h>
 void start_screen();
 void start_game();
 void lanca_bolim(int n, char cancha[n][n]);
 void mostra_cancha(int n, char cancha[n][n]);
 void inicializa_cancha(int n, char cancha[n][n]);
-void mover_ponto(int n, int row, int col, int cancha[n][n]);
+void movimenta_ponto(int n, char cancha[n][n], char c, int row, int col);
 
 
 
 void start_screen() {
+    system("cls");
     printf("\n\n\n");
     printf("%25c****************************\n", ' ');
     printf("%25c*                          *\n",' ');
@@ -25,10 +27,10 @@ void start_screen() {
 
 void start_game()  {
 
-    int cancha[N][N];
-    inicializa_cancha(N, cancha);
-    lanca_bolim(N, cancha);
-    mostra_cancha(N, cancha);
+    system("cls");
+    char cancha[TAMANHO][TAMANHO];
+    inicializa_cancha(TAMANHO, cancha);
+    lanca_bolim(TAMANHO, cancha);
     system("pause");
 
 
@@ -39,9 +41,9 @@ void start_game()  {
 void lanca_bolim(int n, char cancha[n][n]) {
     int col, row;
     char bolim = 'X';
-    col = rand() % N;
-    row = rand() % N;
-    movimenta_ponto(N, cancha, bolim, row, col);
+    col = rand() % n;
+    row = rand() % n;
+    movimenta_ponto(n, cancha, bolim, row, col);
 
 
 }
@@ -62,25 +64,24 @@ void mostra_cancha(int n, char cancha[n][n]) {
 
 void inicializa_cancha(int n, char cancha[n][n]) {
     int i, j;
-    for(i = 0; i < N; i++) {
-        for(j = 0; j < N; j++) {
+    for(i = 0; i < TAMANHO; i++) {
+        for(j = 0; j < TAMANHO; j++) {
             cancha[i][j] = '0';
         }
     }
 }
 
-void movimenta_ponto(N, int cancha[N][N], char c, int row, int col)
-{
-    int i, j;
+void movimenta_ponto(int n, char cancha[n][n], char c, int row, int col) {
+    int i;
+    char bolim = c;
+    cancha[TAMANHO - 1][col] = c; /*Ultima linha, e coluna selecionada aleatoriamente*/
 
-    cancha[N - 1][col] = c;
-
-    for(i = 5; i >= row; i--) {
+    for(i = TAMANHO - 1; i >= row; i--) { /*O bolim começa da última linha e vai subindo até a linha escolhida*/
         system("cls");
-        mostra_cancha(N, cancha);
-        getchar();
-        cancha[i][col] = 0;
-        cancha[i - 1][col] = bolim;
+        mostra_cancha(TAMANHO, cancha);
+        Sleep(300);
+        cancha[i][col] = bolim;
+        cancha[i + 1][col] = '0';
     }
 
 }
